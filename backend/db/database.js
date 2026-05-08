458520101e623eba712c764059ca41f538091189
Sayed Shaizan <sayedshaizan4@gmail.com>
2026-05-08 22:41:14 +0530

    fix db path

--- backend/db/database.js
+++ backend/db/database.js
@@ -1,7 +1,11 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const db = new sqlite3.Database(path.join(__dirname, '../../taskflow.db'));
+const dbPath = process.env.NODE_ENV === 'production' 
+  ? '/tmp/taskflow.db' 
+  : path.join(__dirname, '../../taskflow.db');
+
+const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS users (
